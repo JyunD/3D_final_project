ce1500d5ca72f0dc3a1bad97806ce4a7202c962c
fff321755 <105201032@cc.ncu.edu.tw>
2020-06-11 16:45:13 +0800

    StartMeun
    ver1,0

---
+++ Assets/start_scene/settings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settings : MonoBehaviour
+{
+    public GameObject sampleblock;
+    public Scrollbar SizeBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scale = SizeBar.value * 0.1f + 0.2f;
+        sampleblock.transform.localScale = new Vector3(scale, scale, scale);
+
+
+
+
+    }
+}
